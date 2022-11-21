5ab16fb03d5a72bcbfe58c1ccf5ae262eead65a7
Daniil <danil.beshlebnov@gmail.com>
2022-11-21 17:50:57 +0200

    create questions

--- script.js
+++ script.js
@@ -77,56 +77,21 @@ arr.sort(compareNumber)
  */
 	
 
-const restorantData = {
-    menu: [
-        {
-            name: 'Salad Caesar',
-            price: '1$'
-        },
-        {
-            name: 'Pizza Diavola',
-            price: '9$'
-        },
-        {
-            name: 'Beefsteak',
-            price: '17$'
-        },
-        {
-            name: 'Napoleon',
-            price: '7$'
-        }
-    ],
-    waitors: [
-        {name: 'Alice', age: 22}, {name: 'John', age: 24}
-    ],
-    averageLunchPrice: '20$',
-    openNow: true
-};
+let numberOfFilms = prompt('how many films did you wached?')
 
-function isOpen(prop) {
-    let answer = '';
-    prop ? answer = 'Закрыто' : answer = 'Открыто';
-
-    return answer;
+let personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false
 }
 
-console.log(isOpen(restorantData.openNow))
+const a  = prompt('one of the last film which you watched?')
+b = prompt('how much do you evaluete it?')
+c = prompt('one of the last film which you watched?')
+d = prompt('how much do you evaluete it?')
 
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-    if (+fDish.price.slice(0, -1) + +sDish.price.slice(0, -1) < +average.slice(0, -1)) {
-        return 'Цена ниже средней';
-    } else {
-        return 'Цена выше средней';
-    }
-}
 
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[2], restorantData.averageLunchPrice));
-
-function transferWaitors(data) {
-    const copy = Object.assign({}, data);
-
-    copy.waitors[0] = {name: 'Mike', age: 32};
-    return copy;
-}
-
-transferWaitors(restorantData);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
